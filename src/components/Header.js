bd72503b73ba227e7af194cf940875b9184c9e6d
Glenda Guerrero <glendaguerrerof@gmail.com>
2020-12-16 00:05:00 -0500

    Realizando la aplicacion del clima

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+const Header = ({titulo}) => {
+    return (
+        <div>
+            <nav>
+                <div className='nav-wrapper light-blue darken-2'>
+                <a href="#!" className='brand-logo'>{titulo}</a>
+                </div>
+            </nav>
+        </div>
+    )
+}
+Header.propTypes = {
+    titulo: PropTypes.string.isRequired
+}
+
+
+export default Header
